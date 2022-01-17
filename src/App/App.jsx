646b37d722f3649d52fd5ce9f669c8bf64924258
Mahan Jafari <mahanjafari14@gmail.com>
2022-01-17 08:14:01 +0330

    Customize ChatFeed part

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "../Components/ChatFeed";
 import "./App.css";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         projectID="d645e134-0524-4996-abe8-0083070d42e6"
         userName="Mahan"
         userSecret="123123"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
       />
     </>
   );
